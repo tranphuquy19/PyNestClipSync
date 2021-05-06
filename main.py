b31353d715e40c846fbe6c42e575cf9e92672892
Quy (Christian) P. TRAN <christian@enclave.vn>
2021-05-06 14:00:49 +0700

    temp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import asyncio
 import socketio
-import signal
-import sys
 
 sio = socketio.AsyncClient(reconnection=True, reconnection_attempts=20, reconnection_delay=1)
 rooms = open('rooms.txt', 'r').readlines()
@@ -31,19 +29,7 @@ async def start_server():
     await sio.wait()
 
 
-async def disconnect_server():
-    await sio.disconnect()
-
-
-def interrupt_handler(sig, frame):
-    disconnect_server()
-    print("Disconnected")
-    sys.exit(0)
-
-
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, interrupt_handler)
     loop = asyncio.get_event_loop()
     loop.create_task(start_server())
     loop.run_forever()
-    signal.pause()
