4917938cc84a9113b6c3438d5f907c8f97a54391
tranphuquy19 <christian@enclave.vn>
2021-05-14 16:43:44 +0700

    add view engine ejs && static folder

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) { }
 
+    @Render('index')
     @Get()
-    getHello(): string {
+    getHello(): any {
         return this.appService.getHello();
     }
 }
