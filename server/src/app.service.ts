dfc0de56cccd5c5c52e7023855c83f1687ca3466
tranphuquy19 <christian@enclave.vn>
2021-05-14 16:44:51 +0700

    update

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): any {
-        return { name: 'go-clipsync' };
+        return { name: 'Python NestJS Clipboard Synchronization' };
     }
 }
